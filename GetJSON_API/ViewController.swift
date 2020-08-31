7430f508261fc3a5247bcb34949b4af41d534bca
steven <thaikhacmanh1998@gmail.com>
2020-08-31 16:38:01 +0700

    Initial Commit

---
+++ GetJSON_API/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetJSON_API
+//
+//  Created by V000232 on 8/31/20.
+//  Copyright © 2020 V000232. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
